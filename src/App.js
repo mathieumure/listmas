7a4b3deb3a3d79265482a4d6474e1d46f5f6ac21
Mathieu Mure <mathieu.mure@gmail.com>
2018-12-09 13:05:41 +0100

    feat: setup home page design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+
+const Main = () => (
+  <section className="banner">
+    <h1 className="banner__title">Listmast</h1>
+    <div className="banner__subtitle">Manage your Christmast list</div>
+    <div className="banner__actions">
+      <Link to="/list/new" className="banner__button">Create your list</Link>
+    </div>
+  </section>
+)
+
+const NewList = () => (
+  <h1>Coucou</h1>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Main} />
+          <Route path="/list/new" component={NewList} />
+        </div>
+      </Router>
     );
   }
 }
